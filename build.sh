2f4c7f0f51f1bed66a9b4893cb3edd87fdef9a2a
Logan Roberts <logancroberts@outlook.com>
2024-07-12 15:06:15 -0400

    Add tokei and blueman

--- build.sh
+++ build.sh
@@ -13,57 +13,59 @@ curl -Lo /etc/yum.repos.d/_copr_wezfurlong-wezterm.repo https://copr.fedorainfra
 
 # this installs a package from fedora repos
 rpm-ostree --idempotent install \
-	adobe-source-han-sans-jp-fonts \
-	adobe-source-han-serif-jp-fonts \
-	mozilla-fira-mono-fonts \
-	powerline-fonts \
-	jetbrains-mono-fonts-all \
-	google-droid-sans-mono-fonts \
-	rofi-wayland \
-	kvantum \
-	kvantum-qt5 \
-	qt6ct \
-	qt5ct \
-	onedrive \
-	zathura \
-	zathura-pdf-mupdf \
-	zathura-djvu \
-	zathura-ps \
-	zathura-cb \
-	fcitx5 \
-	fcitx5-configtool \
-	fcitx5-gtk \
-	fcitx5-qt5 \
-	fcitx5-mozc \
-	neovim \
-	eza \
-	ripgrep \
-	zoxide \
-	tmux \
-	iotop \
-	wezterm \
-	btop \
-	direnv \
-	stow
+    adobe-source-han-sans-jp-fonts \
+    adobe-source-han-serif-jp-fonts \
+    mozilla-fira-mono-fonts \
+    powerline-fonts \
+    jetbrains-mono-fonts-all \
+    google-droid-sans-mono-fonts \
+    rofi-wayland \
+    kvantum \
+    kvantum-qt5 \
+    qt6ct \
+    qt5ct \
+    onedrive \
+    zathura \
+    zathura-pdf-mupdf \
+    zathura-djvu \
+    zathura-ps \
+    zathura-cb \
+    fcitx5 \
+    fcitx5-configtool \
+    fcitx5-gtk \
+    fcitx5-qt5 \
+    fcitx5-mozc \
+    neovim \
+    eza \
+    ripgrep \
+    zoxide \
+    tmux \
+    tokei \
+    iotop \
+    wezterm \
+    btop \
+    direnv \
+    stow
 
 # Hyprland Stuff
 rpm-ostree --idempotent install \
-	hyprland \
-	hyprland-contrib \
-	hyprpicker \
-	hyprlock \
-	hypridle \
-	swww \
-	aylurs-gtk-shell \
-	fd-find \
-	polkit-gnome \
-	network-manager-applet \
-	wl-clipboard \
-	brightnessctl \
-	playerctl \
-	kanshi \
-	pavucontrol \
-	kitty # For until wezterm wayland stuff is fixed
+    hyprland \
+    hyprland-contrib \
+    hyprpicker \
+    hyprlock \
+    hypridle \
+    swww \
+    aylurs-gtk-shell \
+    fd-find \
+    polkit-gnome \
+    network-manager-applet \
+    wl-clipboard \
+    brightnessctl \
+    playerctl \
+    kanshi \
+    pavucontrol \
+    blueman \
+    kitty # For until wezterm wayland stuff is fixed
 
 # Example for enabling a System Unit File
 systemctl enable podman.socket
